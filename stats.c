40010f7df490a327a7f7250b1623e092c2dabcc1
KirankumarRajagopalan <91051522+KirankumarRajagopalan@users.noreply.github.com>
2021-10-27 11:39:26 -0700

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,17 +1,17 @@
 #include "stats.h"
 
-
-
-struct Stats compute_statistics(const float* numberset, int setlength) {
-     
-    struct Stats s;
-}
-
 struct Stats s {
     s.average = 0;
     s.min = 0;
     s.max = 0;
 };
+
+
+struct Stats compute_statistics(const float* numberset, int setlength) {
+     
+    struct Stats s;
+};
+
 
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
